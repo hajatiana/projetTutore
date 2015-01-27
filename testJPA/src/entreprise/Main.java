425ca9753c5e3bcc7ca9fe769619b21c6b56b99a
hajatiana <ramhajatiana@gmail.com>
2015-01-27 14:10:40 +0100

    test commit

---
+++ testJPA/src/entreprise/Main.java
@@ -0,0 +1,16 @@
+package entreprise;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Personne p = new Personne();
+		p.setAge(20);
+		p.setNom("Tintin");
+		p.setKey(1);
+		
+		EntityManager entityManager = 
+
+		entityManager.persist(p);
+	}
+
+}
---
+++ testJPA/src/entreprise/Personne.java
@@ -0,0 +1,32 @@
+package entreprise;
+
+@Entity
+public class Personne {
+	
+	private long key;	// clef primaire
+	private int age;
+	private String nom;
+	
+	@Id
+	public long getKey() {
+		return key;
+	}
+	public void setKey(long key) {
+		this.key = key;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+
+}
